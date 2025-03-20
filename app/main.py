8edd464e5fa0f83fe27c96d994731486aa72cf10
Victor Goreanu <victorgoreanu2@gmail.com>
2025-03-20 15:29:20 +0200

    Second Test

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
